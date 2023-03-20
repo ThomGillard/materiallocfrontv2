e0849babca05829e61db5672945987ed6ba0a4e7
Thomas Gillard <119607894+ThomGillard@users.noreply.github.com>
2023-03-20 15:35:27 +0100

    1.01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {Page404Component} from "./components/page404/page404.component";
+import {HomepageComponent} from "./components/homepage/homepage.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'accueil', pathMatch: 'full'},
+  { path: 'accueil', component: HomepageComponent },
+  { path: '404', component: Page404Component },
+  { path: '**', redirectTo: '404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
-import { ConnexionComponent } from './components/connexion/connexion.component';
+import { ConnexionComponent } from './module.authentification/connexion/connexion.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Page404Component } from './components/page404/page404.component';
+import { RegisterComponent } from './module.authentification/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    ConnexionComponent
+    ConnexionComponent,
+    HeaderComponent,
+    FooterComponent,
+    Page404Component,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
