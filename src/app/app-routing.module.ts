38f4081287354fd399d77a438ddb4c345a2197be
Thomas Gillard <119607894+ThomGillard@users.noreply.github.com>
2023-03-20 16:54:05 +0100

    1.02

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {Page404Component} from "./components/page404/page404.component";
 import {HomepageComponent} from "./components/homepage/homepage.component";
+import {ConnexionComponent} from "./module.authentification/connexion/connexion.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'accueil', pathMatch: 'full'},
   { path: 'accueil', component: HomepageComponent },
+  { path: 'connexion', component: ConnexionComponent},
   { path: '404', component: Page404Component },
   { path: '**', redirectTo: '404'}
 ];
