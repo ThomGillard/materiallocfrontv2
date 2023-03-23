ecd7f5175b7f3dbc7b9f7694a627db6c86e3cdf2
Thomas Gillard <119607894+ThomGillard@users.noreply.github.com>
2023-03-23 10:32:29 +0100

    2.02.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {Page404Component} from "./components/page404/page404.component";
 import {HomepageComponent} from "./components/homepage/homepage.component";
-import {LoginComponent} from "./module.authentification/login/login.component";
+import {LoginComponent} from "./auth/login/login.component";
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'accueil', pathMatch: 'full'},
-  { path: '', redirectTo: 'auth/login', pathMatch: 'full'},
-  { path: 'auth', loadChildren: () => import('./module.authentification/auth.module').then( m => m.AuthModule ) },
   { path: 'accueil', component: HomepageComponent },
   { path: 'connexion', component: LoginComponent},
   { path: '404', component: Page404Component },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
-import { LoginComponent } from './module.authentification/login/login.component';
+import { LoginComponent } from './auth/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { Page404Component } from './components/page404/page404.component';
-import { RegisterComponent } from './module.authentification/register/register.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
